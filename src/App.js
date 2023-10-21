c52ab216fc2ca76ab70e40b7f83dfa2a87bbe95f
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2023-10-21 22:06:42 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ButtonAppBar from './components/Navbar';
+
+import RecipeReviewCard from './components/Card';
+import FullWidthGrid from './components/Grid';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ButtonAppBar />
+        <FullWidthGrid />
+
     </div>
   );
 }
